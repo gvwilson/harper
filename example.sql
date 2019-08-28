2274402f6595a8bc876b6d14461b9cf6575a905e
Greg Wilson <gvwilson@third-bit.com>
2019-08-27 20:29:57 -0400

    Database schema updated and commented.

--- example.sql
+++ example.sql
@@ -1,42 +1,38 @@
 -- Example Harper-Lite database for explanatory and testing purposes.
 -- Use `schema.sql` to create tables first.
 
-insert into lesson_anchor values (NULL, "2019-01-01 09:00:00");
+insert into lesson values
+	(NULL, "2019-01-01 09:00:00");
 
-insert into lesson_version values (
-	NULL,
-        1,
-        "2019-01-01 09:00:01",
-        "Tests of Univariate Normality",
-        "How can we tell if univariate data is normally distributed?\nThis lesson describes three tests and explains the strengths and weaknesses of each.",
-        "1.3.1",
-	"http://stats.fringe.tv/stats454/normality/stats454-normality.zip",
-	"https://creativecommons.org/licenses/by-nc/4.0/",
-	"This material was originally developed as part of a sequence on data integrity\nfor seniors and graduate students in statistics, then modified for delivery in\nconference workshops.  The Shapiro-Wilk material feels pretty solid; the ECF\nmaterial has only been used a couple of times, and probably needs more work."
-);
+insert into lesson_version values
+	(NULL,
+         1,
+         "2019-01-01 09:00:01",
+         "Tests of Univariate Normality",
+         "How can we tell if univariate data is normally distributed?\nThis lesson describes three tests and explains the strengths and weaknesses of each.",
+         "1.3.1",
+	 "http://stats.fringe.tv/stats454/normality/stats454-normality.zip",
+	 "https://creativecommons.org/licenses/by-nc/4.0/",
+	 "This material was originally developed as part of a sequence on data integrity\nfor seniors and graduate students in statistics, then modified for delivery in\nconference workshops.  The Shapiro-Wilk material feels pretty solid; the ECF\nmaterial has only been used a couple of times, and probably needs more work.");
 
-insert into person_anchor values
-	(NULL, "2019-01-01 09:00:01")
-;
+insert into person values
+	(NULL, "2019-01-01 09:00:01");
 
-insert into person_version values (
-	NULL,
-        1,
-        "2019-01-01 09:00:02",
-        "Walter Bishop",
-        "Bishop, Walter",
-        "w.bishop@fringe.tv"
-);
+insert into person_version values
+	(NULL,
+         1,
+         "2019-01-01 09:00:02",
+         "Walter Bishop",
+         "Bishop, Walter",
+         "w.bishop@fringe.tv");
 
 insert into contributor values
-	(NULL, 1, 1)
-;
+	(NULL, 1, 1);
 
 insert into objective values
 	(NULL, 1, "Describe the 68-95-99.7 rule and explain why it works and when it fails."),
 	(NULL, 1, "Describe and apply the Shapiro-Wilk test for normality of univariate data."),
-	(NULL, 1, "Describe and apply the ECF test for normality of multivariate data.")
-;
+	(NULL, 1, "Describe and apply the ECF test for normality of multivariate data.");
 
 insert into keyword values
 	(NULL, "2019-01-01 09:00:03", "Normal distribution"),
@@ -52,23 +48,19 @@ insert into keyword values
 	(NULL, "2019-01-01 09:00:03", "ECF test")
 ;
 
-insert into pre_req values
-	(NULL, 1, 1),
-        (NULL, 1, 2),
-        (NULL, 1, 3),
-        (NULL, 1, 4),
-        (NULL, 1, 5),
-        (NULL, 1, 6)
-;
+insert into requisites values
+	(NULL, 1, "pre", 1),
+        (NULL, 1, "pre", 2),
+        (NULL, 1, "pre", 3),
+        (NULL, 1, "pre", 4),
+        (NULL, 1, "pre", 5),
+        (NULL, 1, "pre", 6),
+	(NULL, 1, "post", 7),
+        (NULL, 1, "post", 8),
+        (NULL, 1, "post", 9),
+        (NULL, 1, "post", 10),
+        (NULL, 1, "post", 11);
 
-insert into post_req values
-	(NULL, 1, 7),
-        (NULL, 1, 8),
-        (NULL, 1, 9),
-        (NULL, 1, 10),
-        (NULL, 1, 11)
-;
-
-insert into requirement values
-	(NULL, 1, "R", "nickr", ">=1.2.3.4")
+insert into installation values
+	(NULL, 1, "nickr", ">=1.2.3")
 ;
--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@
 -- * All text fields marked `not null` are expected to be non-empty as well.
 --
 -- * Lessons and people can be updated, and so are represented using a two-part
---   structure: `THING_anchor` as a common reference, and `THING_version` with
+--   structure: `THING` as a common reference, and `THING_version` with
 --   user-visible details.
 --
 -- * "Keywords" are actually phrases used to identify prerequisites (what the
@@ -23,93 +23,131 @@
 --   new (canonical) keywords to that version.
 
 -- Unique identifier for a lesson.
-create table lesson_anchor (
+create table lesson (
+	-- unique ID for each lesson
 	id			integer	not null primary key autoincrement,
+        -- date lesson was first created
 	created			text	not null
 );
 
 -- Details of a specific version of a lesson.
 create table lesson_version (
+	-- globally unique ID for each version of each lesson
 	id			integer	not null primary key autoincrement,
-	lesson_anchor_id	integer	not null,
-	last_updated		integer	not null,
+        -- ID of the lesson that this is a version of
+	lesson_id		integer	not null,
+        -- date this version was created
+	created			integer	not null,
+        -- title of this version of the lesson
 	title			text	not null,
+        -- abstract for this version of the lesson
 	abstract		text	not null,
+        -- lesson's own version ID
 	version			text	not null,
-	package			text,
+        -- URL of downloadable version of lesson material (if any)
+	download		text,
+        -- license for this version of the lesson
 	license			text	not null,
+        -- notes for this version of the lesson
 	notes			text	not null,
-	foreign key(lesson_anchor_id)	references lesson_anchor(id)
+        -- cross-reference to the lesson that this is a version of
+	foreign key(lesson_id)
+	        references lesson(id)
 );
 
 -- Unique identifier for a person.
-create table person_anchor (
+create table person (
+	-- unique ID for each person
 	id			integer	not null primary key autoincrement,
+        -- date person was first created
 	created			text	not null
 );
 
 -- Details of a specific version of a person.
 create table person_version (
+	-- globally unique ID for each version of each person
 	id			integer	not null primary key autoincrement,
-	person_anchor_id	integer	not null,
-	last_updated		integer	not null,
+        -- ID of the person that this is a version of
+	person_id		integer	not null,
+        -- date this version was created
+	created			integer	not null,
+        -- person's name (as presented)
 	name			text	not null,
+        -- person's name (for sorting purposes)
 	sort_name		text	not null,
+        -- person's contact email
 	email			text,
-	foreign key(person_anchor_id)	references person_anchor(id)
+        -- cross-reference to the person that this is a version of
+	foreign key(person_id)
+	        references person(id)
 );
 
 -- Join table connecting people to lessons.
 create table contributor (
+	-- unique ID for this join record
 	id			integer	not null primary key autoincrement,
+        -- lesson version referred to
 	lesson_version_id	integer	not null,
+        -- person version referred to
 	person_version_id	integer	not null,
-	foreign key(lesson_version_id)	references lesson_version(id),
-	foreign key(person_version_id)	references person_version(id)
+        -- cross-references
+	foreign key(lesson_version_id)
+	        references lesson_version(id),
+	foreign key(person_version_id)
+	        references person_version(id)
 );
 
 -- Learning objectives for each version of a lesson.
 create table objective (
+	-- globally unique ID for this learning objective
 	id			integer	not null primary key autoincrement,
+        -- lesson version referred to
 	lesson_version_id	integer	not null,
+        -- learning objective
 	objective		text	not null,
-	foreign key(lesson_version_id)	references lesson_version(id)
+        -- cross-references
+	foreign key(lesson_version_id)
+	        references lesson_version(id)
 );
 
 -- Keywords (actually phrases) used for prereqs and postreqs.
 create table keyword (
+	-- unique ID for this keyword
 	id			integer	not null primary key autoincrement,
+        -- date this keyword was created
 	created			text	not null,
+        -- text of keyword
 	keyword			text	not null
 );
 
--- Join table connecting keywords as prerequisites of a lesson version.
-create table pre_req (
+-- Join table connecting keywords as pre/post-requisites of a lesson version.
+create table requisites (
+	-- unique ID for this join record
 	id			integer	not null primary key autoincrement,
+        -- lesson version referred to
 	lesson_version_id	integer	not null,
+        -- pre- or post-requisite? ("pre", "post")
+        pre_post		text	not null,
+        -- keyword referred to
 	keyword_id		integer	not null,
-	foreign key(keyword_id)		references keywords(id)
-	foreign key(lesson_version_id)	references lesson_version(id)
-);
-
--- Join table connecting keywords as postrequisites of a lesson version.
-create table post_req (
-	id			integer	not null primary key autoincrement,
-	lesson_version_id	integer	not null,
-	keyword_id		integer	not null,
-	foreign key(keyword_id)		references keywords(id)
-	foreign key(lesson_version_id)	references lesson_version(id)
+        -- cross-references
+	foreign key(lesson_version_id)
+	        references lesson_version(id),
+	foreign key(keyword_id)
+	        references keywords(id)
 );
 
 -- Software requirements for each lesson version.
--- `major` is the major heading (e.g., "R" or "Python").
--- `minor` is the minor requirement (e.g., the package name).
--- `details` is optional extra information (e.g., a version specification).
-create table requirement (
+create table installation (
+	-- unique ID for this software requirement
 	id			integer	not null primary key autoincrement,
+        -- ID of lesson version
         lesson_version_id	integer	not null,
-	major			text	not null,
-	minor			text	not null,
+        -- thing required (e.g., R, lubridate)
+	requirement		text	not null,
+        -- details (e.g., package version)
         details			text,
-	foreign key(lesson_version_id)	references lesson_version(id)
+        -- cross-references
+	foreign key(lesson_version_id)
+	        references lesson_version(id)
 );
